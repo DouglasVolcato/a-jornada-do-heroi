1bc05e6a13869247e125c1253179037d09ba90dc
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-25 15:34:16 -0300

    feat: add amanda factory

---
+++ assets/scripts/presentation/factories/entities/characters/friends/amanda-factory.js
@@ -0,0 +1,22 @@
+function makeAmandaFactory() {
+  const amanda = new MainEntity(
+    "Amanda",
+    "Sua melhor amiga",
+    1,
+    100,
+    20,
+    10,
+    20,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    [],
+    []
+  );
+
+  return amanda;
+}
