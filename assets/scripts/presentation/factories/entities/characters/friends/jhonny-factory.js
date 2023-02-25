62f82b8b729d14a89728c34d765af8a7c9a32088
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-25 15:34:24 -0300

    feat: add jhonny factory

---
+++ assets/scripts/presentation/factories/entities/characters/friends/jhonny-factory.js
@@ -0,0 +1,22 @@
+function makeJhonnyFactory() {
+  const jhonny = new MainEntity(
+    "Jhonny",
+    "Seu melhor amigo",
+    1,
+    100,
+    20,
+    10,
+    20,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    [],
+    []
+  );
+
+  return jhonny;
+}
