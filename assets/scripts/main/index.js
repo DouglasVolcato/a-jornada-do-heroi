b768229f1cf84f3d490993dcca3564c4a694d3d7
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-26 20:23:44 -0300

    chore: add interation example

--- assets/scripts/main/index.js
+++ assets/scripts/main/index.js
@@ -1,17 +1,11 @@
-////BATTLE
-// const playerName = "Douglas";
-// const playerDescription = "Descrição teste";
-// const player = makePlayerFactory(playerName, playerDescription);
-// const gameStatus = makeGameStatusFactory(player);
-// const enemy = makeGoblinFactory();
-// const battle = makeBattleControllerFactory(enemy, gameStatus);
-// battle.start();
+//BATTLE
+const player = makePlayerFactory("", "");
+const gameStatus = makeGameStatusFactory(player);
+const enemy = makeGoblinFactory();
+const battle = makeBattleControllerFactory(enemy, gameStatus);
 
 //HISTORY
-const playerName = "Douglas";
-const playerDescription = "Descrição teste";
-const player = makePlayerFactory(playerName, playerDescription);
-const gameStatus = makeGameStatusFactory(player);
-const history = chapter0;
-const historyInteract = makeHistoryControllerFactory(history, gameStatus);
-historyInteract.start();
+const part1 = makeHistoryControllerFactory(chapter1, gameStatus, battle);
+const part0 = makeHistoryControllerFactory(chapter0, gameStatus, part1);
+
+part0.start();
