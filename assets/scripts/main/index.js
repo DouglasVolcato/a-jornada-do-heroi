12c836003c0e794e8c12accf18d83f5c06938a8d
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-26 22:27:30 -0300

    feat: add chapter 1

--- assets/scripts/main/index.js
+++ assets/scripts/main/index.js
@@ -1,11 +1,27 @@
-//BATTLE
 const player = makePlayerFactory();
 const gameStatus = makeGameStatusFactory(player);
-const enemy = makeGoblinFactory();
-const battle = makeBattleControllerFactory(enemy, gameStatus);
 
-//HISTORY
-const part1 = makeHistoryControllerFactory(part_1, gameStatus, battle);
-const part0 = makeHistoryControllerFactory(part_0, gameStatus, part1);
+//PART 1
+const part1_3 = makeHistoryControllerFactory(part_r1, gameStatus);
+const part1_2 = makeBattleControllerFactory(
+  makeAmandaFactory(),
+  gameStatus,
+  part1_3
+);
+const part1_1 = makeBattleControllerFactory(
+  makeJhonnyFactory(),
+  gameStatus,
+  part1_3
+);
+const part_1 = makeHistoryControllerFactory(
+  part1,
+  gameStatus,
+  "",
+  part1_1,
+  part1_2
+);
 
-part0.start();
+//PART 0
+const part_0 = makeHistoryControllerFactory(part0, gameStatus, part_1);
+
+part_0.start();
